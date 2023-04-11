c921c998acd91677687c9f1168f1b85d4916f13d
Kaudjhis Eba Sonia Kouassi <sonialastar2008@gmail.com>
2023-04-11 19:27:04 -0400

    Add Style css Add index html modify index.js modify package.json

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 // index.js
-const express = require('express')
+'use strict';
 
+var fs = require('fs');
+const express = require('express')
 const app = express()
+
 const PORT = 4000
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.route('/_api/package.json')
+  .get(function(req, res, next) {
+    console.log('requested');
+    fs.readFile(__dirname + '/package.json', function(err, data) {
+      if(err) return next(err);
+      res.type('txt').send(data.toString());
+    });
+  });
+  
+app.route('/')
+    .get(function(req, res) {
+		  res.sendFile(process.cwd() + '/views/index.html');
+    })
+
+// Respond not found to all the wrong routes
+app.use(function(req, res, next){
+  res.status(404);
+  res.type('txt').send('Not found');
+});
+
+// Error Middleware
+app.use(function(err, req, res, next) {
+  if(err) {
+    res.status(err.status || 500)
+      .type('txt')
+      .send(err.message || 'SERVER ERROR');
+  }  
 })
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
-
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+//Listen on port set in environment variable or default to 3000
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Node.js listening on port " + listener.address().port);
+});
 
 // Export the Express API
 module.exports = app
